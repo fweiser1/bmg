fd3a717bcf7a471540f4c7db063948054a36a5bb
Administrateur <Administrateur@6212-D.cite-schuman.edu>
2018-10-16 14:01:28 +0200

    Code controleur c_gererGenres.php (récupération de $action)

---
+++ controleurs/c_gererGenres.php
@@ -0,0 +1,39 @@
+<?php
+
+if(!isset($_REQUEST['action']))
+{
+    $action = 'listerGenres';
+}
+else
+{
+    $action = $_REQUEST['action'];
+}
+
+// varaiables pour la gestion des messages
+$msg = ''; //message passé à la vue v_afficherMessage
+$lien = ''; //message passé à la vue v_afficherErreurs
+
+switch ($action) 
+{
+    case 'listerGenres' : 
+    {        
+    }
+    break;
+    case 'consulterGenre' : 
+    {        
+    }
+    break;
+    case 'ajouterGenre' : 
+    {        
+    }
+    break;
+    case 'modifierGenre' : 
+    {        
+    }
+    break;
+    case 'supprimerGenre' : 
+    {        
+    }
+    break;
+    default : include "vues/v_home.php";
+}
